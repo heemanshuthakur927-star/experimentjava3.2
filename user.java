637b68d3553abf518a858b66ca34cccd3c0d0bab
heemanshuthakur927-star <heemanshuthakur927@gmail.com>
2025-10-30 19:43:10 +0530

    Create user.java

---
+++ user.java
@@ -0,0 +1,31 @@
+package com.example.pblj.entity;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String email;
+
+    public User() {}
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+    public void setName(String name) { this.name = name; }
+    public void setEmail(String email) { this.email = email; }
+
+    @Override
+    public String toString() {
+        return id + " | " + name + " | " + email;
+    }
+}
